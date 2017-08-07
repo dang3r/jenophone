d8cc64d053fb3dd489841a4dfb3db910cd3fddd9
Daniel Cardoza <daniel.cardoza@aerofs.com>
2017-08-06 22:24:44 -0700

    - removed unneeded token, api - add logging statements

--- jenophone.go
+++ jenophone.go
@@ -14,45 +14,34 @@ type Resp struct {
 
 type Message struct {
 	Body string `xml:"Body"`
-	From string `xml:"From,attr"`
-	To   string `xml:"To,attr"`
+	From string `xml:"from,attr"`
+	To   string `xml:"to,attr"`
 }
 
 func main() {
 
 	// Config
 
-	var accountSid string
 	var num1, num2 string
-	flag.StringVar(&accountSid, "accountsid", "", "Twilio Account Sid")
 	flag.StringVar(&num1, "num1", "", "First phone number")
 	flag.StringVar(&num2, "num2", "", "Second phone number")
 	flag.Parse()
-	if accountSid == "" || num1 == "" || num2 == "" {
-		log.Fatalf("accountsid, token, num1, num2 are required CLI parameters\n")
+	if num1 == "" || num2 == "" {
+		log.Fatalf("num1, num2 are required CLI parameters\n")
 	}
 
-	//
 	// Handlers
-	//
 
-	// Handle and forward an incoming SMS message
 	smsHandler := func(w http.ResponseWriter, r *http.Request) {
 		if r.Method != "GET" {
 			http.Error(w, "Unsupported method", 405)
 			return
 		}
-		log.Println(r.URL.Query())
-		log.Println(r.Header)
-
 		from := r.URL.Query().Get("From")
 		to := r.URL.Query().Get("To")
 		body := r.URL.Query().Get("Body")
-		toAccountSid := r.URL.Query().Get("AccountSid")
-		if accountSid != toAccountSid {
-			http.Error(w, "bad twilio account", 400)
-			return
-		}
+		log.Println(r.URL.Query())
+		log.Println(r.Header)
 
 		var forwardNum string
 		switch from {
@@ -64,18 +53,18 @@ func main() {
 			http.Error(w, "Bad request", 400)
 			return
 		}
+		log.Printf("Received from %v and going to %v\n", from, forwardNum)
 		resp := Resp{Message: Message{Body: body, From: to, To: forwardNum}}
 		bytes, err := xml.Marshal(resp)
 		if err != nil {
 			http.Error(w, err.Error(), 500)
 			return
 		}
-
+		log.Println("Sending back", string(bytes))
 		w.Header().Set("Content-Type", "application/xml")
 		w.Write(bytes)
 	}
 
-	// Service healthcheck
 	healthcheckHandler := func(w http.ResponseWriter, r *http.Request) {
 		if r.Method != "GET" {
 			http.Error(w, "Unsupported method", 405)
@@ -86,5 +75,5 @@ func main() {
 
 	http.HandleFunc("/sms", smsHandler)
 	http.HandleFunc("/healthcheck", healthcheckHandler)
-	http.ListenAndServeTLS(":443", "cert.pem", "privkey.pem", nil)
+	http.ListenAndServeTLS(":443", "fullchain.pem", "privkey.pem", nil)
 }
